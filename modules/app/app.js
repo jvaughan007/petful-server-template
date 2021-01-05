09f9593f25b8938fc04e42e8f9a2c54cf5cb3262
Joshua Vaughan <jvaughan007@yahoo.com>
2021-01-04 23:28:59 -0600

    fixing deployment

--- modules/app/app.js
+++ modules/app/app.js
@@ -3,9 +3,11 @@ const cors = require('cors');
 const app = express();
 const { CLIENT_ORIGIN } = require('../../config.js');
 
-app.use(cors({
-    origin: CLIENT_ORIGIN
-  });
+app.use(
+    cors({
+        origin: process.env.CLIENT_ORIGIN || 'http://localhost:3000'
+    })
+);
 
 app.use('/people', require('../people/people.router'));
 app.use('/pets', require('../pets/pets.router'));
