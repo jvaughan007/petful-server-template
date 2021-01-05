70b341769ddbfef673e2b99fa1546c86313e6931
Joshua Vaughan <jvaughan007@yahoo.com>
2021-01-05 13:37:19 -0600

    debugging deployment

--- modules/app/app.js
+++ modules/app/app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 const { CLIENT_ORIGIN } = require('../../config.js');
+console.log('app.js CLIENT_ORIGIN ', CLIENT_ORIGIN);
 
 app.use(
     cors({
